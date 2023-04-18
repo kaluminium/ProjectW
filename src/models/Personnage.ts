01124a0996741411a607c6a217e4a4bbc19c1155
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-04-18 11:19:26 +0200

    ajout de getter et de register

--- src/models/Personnage.ts
+++ src/models/Personnage.ts
@@ -1,18 +1,25 @@
 import {Compte} from "./Compte";
+import {ListePersonnage} from "./ListePersonnage";
+import {BDDConnexion} from "./BDDConnexion";
 
 class Personnage{
     private name : string;
     private xp : number;
     private id : number;
-    private divinity : Divinite;
+    private divinity : string;
     private account : Compte;
     private success : Succes[];
     private inventory : Inventaire;
-    private race : Race;
+    private race : string
     private sex : string;
 
-    constructor(name: string, divinity : string, race : string, sex : string) {
-
+    constructor(id : number, name: string, divinity : string, race : string, sex : string, xp : number) {
+        this.id = id;
+        this.name = name;
+        this.divinity = divinity;
+        this.race = race;
+        this.sex = sex;
+        this.xp = xp;
     }
 
     public getName() : string{
@@ -30,6 +37,30 @@ class Personnage{
 
     public static putCapitalLetter(name : string) : string{
         return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    }
+
+    public static async register(id : number, name : string, sex : string, divinity : string, race : string){
+        if(!Personnage.verifyName(name)) throw new Error("Le nom du personnage n'est pas valide");
+        name = Personnage.putCapitalLetter(name);
+        let listePersonnage = await ListePersonnage.getListePersonnage(id);
+        if(listePersonnage.isFull()) throw new Error("Vous avez atteint le nombre maximum de personnage");
+        const bdd = await BDDConnexion.getInstance();
+        await bdd.query("INSERT INTO `personnage` (`account_id`, `name`, `sex`, `divinity`, `race`) VALUES (?, ?, ?, ?, ?)", [id, name, sex, divinity, race]);
+    }
+
+    public static async getPersonnage(id : number) : Promise<Personnage>{
+        const bdd = await BDDConnexion.getInstance();
+        const result = await bdd.query("SELECT * FROM `personnage` WHERE `id` = ?", [id]);
+        if(result.length > 0){
+            let id = result[0].id;
+            let name = result[0].name;
+            let divinity = result[0].divinity;
+            let race = result[0].race;
+            let sex = result[0].sex;
+            let xp = result[0].xp;
+            return new Personnage(id, name, divinity, race, sex, xp);
+        }
+        throw new Error("Le personnage n'existe pas");
     }
 }
 export{Personnage};
