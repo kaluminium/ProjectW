17a0f3e4d334fc43b25d75e5e1c3db9a8864f53a
Yodwaae <lucas.spitz.drevet@gmail.com>
2023-05-07 15:13:03 +0200

    Début de la commande combat
    J'ai besoin de changements fait sur la branche de Luka que je n'ai pas donc je fais ce commit pour pas perdre mes changements quand je merge

--- src/slashCommands/combat.ts
+++ src/slashCommands/combat.ts
@@ -1,14 +1,33 @@
 import {SlashCommand} from "../types";
 import {CommandInteraction} from "discord.js";
+import {Compte} from "../models/Compte";
+import {Personnage} from "../models/Personnage";
+import {ListePersonnage} from "../models/ListePersonnage";
 
 export const command: SlashCommand = {
     name: "combat",
     usage: "",
     category: "gameplay",
     description: "Permet de lancer un combat",
-    execute(interaction: CommandInteraction): Promise<any> {
 
-    return Promise.resolve(undefined);
+    execute: async (interaction) => {
+        let compte: Compte;
+        let personnage : Personnage;
+        const listePersonnages : ListePersonnage = await compte.getListPersonnage();
+
+        //Récupère le compte du joueur
+        //Erreur(s) possible :
+        //- L'utilisateur n'a pas de compte : gérée, indique à l'utilisateur qu'il n'a pas de compte et lui propose d'en créer un
+        try {
+            compte = await Compte.getAccount(interaction.user.id)
+        } catch (e) {
+            return await interaction.reply({content: `Vous n'avez pas de compte, faites /creation_compte pour créer un compte`, ephemeral: true});
+        }
+
+        //Vérifie que l'utilisateur a des personnages, sinon lui propose d'en créer un puis de le sélectionner
+        //if(listePersonnages.isEmpty()) return await interaction.reply({content: `Vous n'avez pas de personnage, faites /creation_personnage pour créer un personnage,\npuis /select_personnage pour sélectionner un personnage`, ephemeral: true});
+
+
     }
 
 }
