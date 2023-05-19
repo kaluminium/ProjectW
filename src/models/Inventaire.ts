fcc702a4053b7987fd82b7a86e65bb3bcc5a1032
Luka Lindberg <luka.lindberg@etu.univ-lyon1.fr>
2023-05-19 19:45:35 +0200

    ajout des imports backpack et equipment

--- src/models/Inventaire.ts
+++ src/models/Inventaire.ts
@@ -1,4 +1,7 @@
-class Inventaire{
+import {BackPack} from "./BackPack";
+import {Equipment} from "./Equipment";
+
+export class Inventaire{
     private backpack : BackPack;
     private equipment : Equipment;
 }
